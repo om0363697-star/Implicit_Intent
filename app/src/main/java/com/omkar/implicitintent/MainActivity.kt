688e917e65dae61a748aab8bd8f59551b52a9047
Omkar Jaiswal <om0363697@gmail.com>
2026-07-02 16:21:36 +0530

    This is my second commoit

---
+++ app/src/main/java/com/omkar/implicitintent/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.omkar.implicitintent
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+      val webButton= findViewById<CardView>(R.id.carweb)
+        val cameraButton= findViewById<CardView>(R.id.cardcamera)
+
+        webButton.setOnClickListener {
+            val intent= Intent(Intent.ACTION_VIEW)
+            intent.data= Uri.parse("https://thatsthem.com/people-search")
+            startActivity(intent)
+
+        }
+        cameraButton.setOnClickListener {
+            val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+
+        }
+    }
+}
